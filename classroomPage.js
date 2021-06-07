d2ce7d3a20ffefd1cfb197632a5521207119e101
Mappy <40239457+AmiMappy@users.noreply.github.com>
2021-06-07 19:24:10 +0530

    Fix a lot about classroom pages handling.
    The code before was just wrong. Now, although it has a few unnecessary checks, it at least works as expected. Previously, the code wouldn't reload the page if the meetAddr was not set, i.e, the link didn't exist.

--- classroomPage.js
+++ classroomPage.js
@@ -1,13 +1,23 @@
 var meetAddr;
-setTimeout(() => {
-  meetAddr = document.getElementsByClassName("QRiHXd")[7].innerText;
-  console.log(meetAddr);
-  if (meetAddr.indexOf("meet.google.com/") !== -1) {
-    chrome.runtime.sendMessage(
-      { meetingExists: true, meetAddr: meetAddr }, (response) => {
-        // console.log(response);
-      });
-  } else {
-    window.location.reload();
+var meetTabOpened = false;
+setTimeout(() => {    
+  if(document.URL.length <= 47){
+    try {
+      meetAddr = document.getElementsByClassName("QRiHXd")[7].innerText;
+    } catch(err){
+      if(meetTabOpened === false){
+        window.location.reload();
+      }
+    }
+    if ((meetAddr.indexOf("meet.google.com/") !== -1) && (meetTabOpened === false)) {
+      chrome.runtime.sendMessage(
+        { meetingExists: true, meetAddr: meetAddr }, (response) => {          
+          meetTabOpened = true;
+        });
+    } else {
+      if(meetTabOpened === false){
+        window.location.reload();
+      }
+    }
   }
 }, 10000);
