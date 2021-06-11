aee0dd99ae65df748a903409fe1eeeb9cf44a92d
Mappy <40239457+AmiMappy@users.noreply.github.com>
2021-06-11 14:25:41 +0530

    Update popup.is to not send accountNo.

--- popup/popup.js
+++ popup/popup.js
@@ -3,5 +3,5 @@ var settings_form = document.getElementById("settings_form");
 
 settings_form.addEventListener('submit', function(e){
     e.preventDefault();    
-    chrome.runtime.sendMessage({account_no: document.getElementById("account_no_input_id").value, notif_method: document.getElementById("notif_method_input_id").value}, (response) => { ; });
+    chrome.runtime.sendMessage({notif_method: document.getElementById("notif_method_input_id").value}, (response) => { ; });
 })
