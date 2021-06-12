087fef6bef782d3c3cb6be69856a16e67113444b
Mappy <40239457+AmiMappy@users.noreply.github.com>
2021-06-12 15:46:12 +0530

    accntchsrpg.js now works every time
    Earlier, it only checked for when the account chooser page was done loading. However, it seems that the first time that page is loaded(for example when the account chooser page is not in the user's history), the 'load' event doesn't trigger. So this is a one-size-fits-all rubbish code fix that simply waits for 5 seconds and then sends the message.

--- accountChooserPage.js
+++ accountChooserPage.js
@@ -1,7 +1,7 @@
-window.addEventListener('load', () => {
+setInterval(() => {
     if(document.URL.indexOf("meet.google.com") !== -1){
         chrome.runtime.sendMessage({accountChooserOpened: true}, (response) => { 
             //console.log(response)
         });
     }
-})
+}, 5000);
