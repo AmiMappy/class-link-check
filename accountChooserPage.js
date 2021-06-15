9cc4e765fcd114d89fa31d949e4e848dedeaeb5b
Mappy <40239457+AmiMappy@users.noreply.github.com>
2021-06-15 14:14:28 +0530

    Update accountChooserPage.js

--- accountChooserPage.js
+++ accountChooserPage.js
@@ -1,5 +1,3 @@
-window.addEventListener("load", () => {
-    if(document.URL.indexOf("meet.google.com") !== -1){        
-        chrome.runtime.sendMessage({accountChooserOpened: true}, (response) => { ; });
-    }
-});
+if(document.URL.indexOf("meet.google.com") !== -1){
+    chrome.runtime.sendMessage({accountChooserOpened: true}, (response) => { ; });
+}
