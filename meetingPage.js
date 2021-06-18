69ed32e0a71383d544deb74169a88c670763f007
Mappy <40239457+AmiMappy@users.noreply.github.com>
2021-06-18 08:24:31 +0530

    Update meetingPage.js
    It could be that none of these fixes with meetingPage.js and accountChooserPage.js actually work. I think that is because if for the first time in a long time(need specifics) the meeting or account choosing pages are loaded, then they just don't respond to window.addEventListener('load') and/or setTimeout(). From what I know, we can do without checking if those particular pages have loaded as the extension sees that for itself before mounting the content script in the first place. Or i am wrong again.

--- meetingPage.js
+++ meetingPage.js
@@ -1,16 +1,13 @@
-var meetingActive;
-window.addEventListener("load", () => {
-  meetingActive = false;
-  if (document.title.indexOf("Meet - ") !== -1) {
-    meetingActive = true;
-    chrome.runtime.sendMessage({ meetingActive: true }, (response) => { 
-    // console.log(response) 
-    });
-  }
-  if (meetingActive === false) {
-    // chrome.runtime.sendMessage({ meetingActive: false }, (response) => { ; });
-    setTimeout(() => {
-      window.location.reload();
-    }, 10000);
-  }
-});
+var meetingActive = false;
+if (document.title.indexOf("Meet - ") !== -1) {
+  meetingActive = true;
+  chrome.runtime.sendMessage({ meetingActive: true }, (response) => { 
+  // console.log(response) 
+  });
+}
+if (meetingActive === false) {
+  // chrome.runtime.sendMessage({ meetingActive: false }, (response) => { ; });
+  setTimeout(() => {
+    window.location.reload();
+  }, 10000);
+}
