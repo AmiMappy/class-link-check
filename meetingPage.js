1bd4b5d44aadf1a9b5e7db665d21d77c6acd73fe
Mappy <40239457+AmiMappy@users.noreply.github.com>
2021-06-19 09:19:22 +0530

    Update meetingPage.js

--- meetingPage.js
+++ meetingPage.js
@@ -1,13 +1,12 @@
-var meetingActive = false;
-if (document.title.indexOf("Meet - ") !== -1) {
-  meetingActive = true;
-  chrome.runtime.sendMessage({ meetingActive: true }, (response) => { 
-  // console.log(response) 
-  });
-}
-if (meetingActive === false) {
-  // chrome.runtime.sendMessage({ meetingActive: false }, (response) => { ; });
-  setTimeout(() => {
-    window.location.reload();
-  }, 10000);
-}
+window.addEventListener('load', () => {
+  if (document.title.indexOf("Meet - ") !== -1) {
+    meetingActive = true;
+    chrome.runtime.sendMessage({ meetingActive: true }, (response) => { 
+    // console.log(response) 
+    });
+  } else {
+    setTimeout(() => {
+      window.location.reload();
+    }, 10000);
+  }
+})
